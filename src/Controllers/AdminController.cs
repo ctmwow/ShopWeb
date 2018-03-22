49c4ed9eb327abee37c8e87154fc15fa641434dc
PatricNox <eronoxsmail2@gmail.com>
2018-03-22 16:40:17 +0100

    [C#] Implement AdminCP

---
+++ src/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using Dapper;
+
+namespace src.Controllers
+{
+    public class AdminController : Controller
+    {
+        private List<Models.ShopModel> products;
+        private String connectionString;
+
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+
+        // Database Fetch
+        public AdminController(IConfiguration configuration)
+        {
+            this.connectionString = configuration.GetConnectionString("ConnectionString");
+            using (var connection = new MySqlConnection(this.connectionString))
+            {
+                this.products = connection.Query<Models.ShopModel>("SELECT * FROM products").ToList();
+            }
+        }
+    }
+}
